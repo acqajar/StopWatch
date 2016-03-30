9fc018ff515ea793f3dc9af101460cd3a2b2c02f
Arsames Qajar <acqajar@gmail.com>
2016-03-30 05:03:15 -0700

    stopwatch

--- NavigationBar/ViewController.swift
+++ NavigationBar/ViewController.swift
@@ -10,9 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var time = 0
+    var timer = NSTimer()
+    var min = 0
+    var hour = 0
+    var sec = 0
+    
+    
+    @IBAction func pauseThis(sender: AnyObject) {
+        timeStopped.text = "\(time) seconds"
+        timer.invalidate()
+    }
+    
+    @IBAction func startThis(sender: AnyObject) {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.result), userInfo: nil, repeats: true)
+        
+        timeStopped.text = "Go!"
+    }
+    
+    
+    
+    @IBOutlet var timeStopped: UILabel!
+    
+    
+    @IBOutlet var currentTime: UILabel!
+    
+    
+    
+    
+    
+    
+    @IBAction func stopTime(sender: AnyObject) {
+        timeStopped.text = "\(time) seconds"
+        timer.invalidate()
+        time = 0
+        currentTime.text = "\(time) seconds"
+        
+    }
+    
+    
+    
+    
+    func result() {
+        time+=1
+        print("\(time) has passed")
+        
+         sec = time % 60
+         min = (time / 60) % 60
+         hour = time / 3600
+        
+        currentTime.text = "\(hour): \(min): \(sec) seconds"
+        
+
+    }
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        var timer = NSTimer()
+//        
+//        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(ViewController.result), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
